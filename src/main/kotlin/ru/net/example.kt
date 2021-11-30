64785f2d6b58684efd3004d7b9e05d9fea9db286
Arsenii Kharlanow <arsenyzp@gmail.com>
2021-11-30 20:47:20 +0300

    init

---
+++ src/main/kotlin/ru/net/example.kt
@@ -0,0 +1,7 @@
+package ru.net
+
+fun test() {
+    if ("" == NORMAL) {
+
+    }
+}
---
+++ src/main/kotlin/ru/net/main.kt
@@ -0,0 +1,9 @@
+package ru.net
+const val NORMAL = "normal+"
+private const val SILVER = "silver"
+private const val GOLD = "gold"
+
+
+fun main(args: Array<String>) {
+
+}
